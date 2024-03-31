d5a3d2a53b50d1a6282a0dd8176736a07fae0f93
chrismpettyjohn <chrismpettyjohn@gmail.com>
2024-03-31 17:02:38 -0400

    feat: remove legacy group fts

--- nitro-ui/src/components/groups/views/GroupInformationView.tsx
+++ nitro-ui/src/components/groups/views/GroupInformationView.tsx
@@ -1,10 +1,7 @@
-import { GroupInformationParser, GroupRemoveMemberComposer, GroupState } from '@nitro-rp/renderer';
+import { GroupInformationParser } from '@nitro-rp/renderer';
 import { FC } from 'react';
-import { CatalogPageName, CreateLinkEvent, GetGroupManager, GetGroupMembers, GetSessionDataManager, GroupMembershipType, LocalizeText, SendMessageComposer, TryJoinGroup, TryVisitRoom } from '../../../api';
-import { Button, Column, Flex, Grid, GridProps, LayoutBadgeImageView, Text } from '../../../common';
-import { useNotification } from '../../../hooks';
-
-const STATES: string[] = [ 'regular', 'exclusive', 'private' ];
+import { CatalogPageName, CreateLinkEvent, GetGroupManager, GetGroupMembers, LocalizeText, TryVisitRoom } from '../../../api';
+import {Column, Flex, Grid, GridProps, LayoutBadgeImageView, Text } from '../../../common';
 
 interface GroupInformationViewProps extends GridProps
 {
@@ -16,61 +13,6 @@ interface GroupInformationViewProps extends GridProps
 export const GroupInformationView: FC<GroupInformationViewProps> = props =>
 {
     const { groupInformation = null, onClose = null, overflow = 'hidden', ...rest } = props;
-    const { showConfirm = null } = useNotification();
-
-    const isRealOwner = (groupInformation && (groupInformation.ownerName === GetSessionDataManager().userName));
-
-    const joinGroup = () => (groupInformation && TryJoinGroup(groupInformation.id));
-
-    const leaveGroup = () =>
-    {
-        showConfirm(LocalizeText('group.leaveconfirm.desc'), () =>
-        {
-            SendMessageComposer(new GroupRemoveMemberComposer(groupInformation.id, GetSessionDataManager().userId));
-
-            if(onClose) onClose();
-        }, null);
-    }
-
-    const getRoleIcon = () =>
-    {
-        if(groupInformation.membershipType === GroupMembershipType.NOT_MEMBER || groupInformation.membershipType === GroupMembershipType.REQUEST_PENDING) return null;
-
-        if(isRealOwner) return <i className="icon icon-group-owner" title={ LocalizeText('group.youareowner') } />;
-
-        if(groupInformation.isAdmin) return <i className="icon icon-group-admin" title={ LocalizeText('group.youareadmin') } />;
-
-        return <i className="icon icon-group-member" title={ LocalizeText('group.youaremember') } />;
-    }
-
-    const getButtonText = () =>
-    {
-        if(isRealOwner) return 'group.youareowner';
-
-        if(groupInformation.state === GroupState.PRIVATE && groupInformation.membershipType !== GroupMembershipType.MEMBER) return '';
-
-        if(groupInformation.membershipType === GroupMembershipType.MEMBER) return 'group.leave';
-
-        if((groupInformation.membershipType === GroupMembershipType.NOT_MEMBER) && groupInformation.state === GroupState.REGULAR) return 'group.join';
-
-        if(groupInformation.membershipType === GroupMembershipType.REQUEST_PENDING) return 'group.membershippending';
-
-        if((groupInformation.membershipType === GroupMembershipType.NOT_MEMBER) && groupInformation.state === GroupState.EXCLUSIVE) return 'group.requestmembership';
-    }
-
-    const handleButtonClick = () =>
-    {
-        if((groupInformation.state === GroupState.PRIVATE) && (groupInformation.membershipType === GroupMembershipType.NOT_MEMBER)) return;
-
-        if(groupInformation.membershipType === GroupMembershipType.MEMBER)
-        {
-            leaveGroup();
-
-            return;
-        }
-
-        joinGroup();
-    }
 
     const handleAction = (action: string) =>
     {
@@ -112,28 +54,16 @@ export const GroupInformationView: FC<GroupInformationViewProps> = props =>
                     { groupInformation.isOwner &&
                         <Text small underline pointer onClick={ () => handleAction('manage') }>{ LocalizeText('group.manage') }</Text> }
                 </Column>
-                { getRoleIcon() }
             </Column>
             <Column size={ 9 } justifyContent="between" overflow="auto">
                 <Column overflow="hidden">
                     <Column gap={ 1 }>
                         <Flex alignItems="center" gap={ 2 }>
                             <Text bold>{ groupInformation.title }</Text>
-                            <Flex gap={ 1 }>
-                                <i className={ 'icon icon-group-type-' + groupInformation.state } title={ LocalizeText(`group.edit.settings.type.${ STATES[groupInformation.state] }.help`) } />
-                                { groupInformation.canMembersDecorate &&
-                                    <i className="icon icon-group-decorate" title={ LocalizeText('group.memberscandecorate') } /> }
-                            </Flex>
                         </Flex>
                         <Text small>{ LocalizeText('group.created', [ 'date', 'owner' ], [ groupInformation.createdAt, groupInformation.ownerName ]) }</Text>
                     </Column>
                     <Text small overflow="auto" className="group-description">{ groupInformation.description }</Text>
-                </Column>
-                <Column>
-                    { (groupInformation.state !== GroupState.PRIVATE || groupInformation.state === GroupState.PRIVATE && groupInformation.membershipType === GroupMembershipType.MEMBER) &&
-                        <Button disabled={ (groupInformation.membershipType === GroupMembershipType.REQUEST_PENDING) || isRealOwner } onClick={ handleButtonClick }>
-                            { LocalizeText(getButtonText()) }
-                        </Button> }
                 </Column>
             </Column>
         </Grid>
--- nitro-ui/src/components/groups/views/GroupRoomInformationView.tsx
+++ nitro-ui/src/components/groups/views/GroupRoomInformationView.tsx
@@ -1,16 +1,12 @@
-import { DesktopViewEvent, GetGuestRoomResultEvent, GroupInformationComposer, GroupInformationEvent, GroupInformationParser, GroupRemoveMemberComposer, GroupState, HabboGroupDeactivatedMessageEvent, RoomEntryInfoMessageEvent } from '@nitro-rp/renderer';
-import { FC, useState } from 'react';
-import { FaChevronDown, FaChevronUp } from 'react-icons/fa';
-import { GetGroupInformation, GetGroupManager, GetSessionDataManager, GroupMembershipType, LocalizeText, SendMessageComposer, TryJoinGroup } from '../../../api';
-import { Base, Button, Column, Flex, LayoutBadgeImageView, Text } from '../../../common';
-import { useMessageEvent, useNotification } from '../../../hooks';
+import { DesktopViewEvent, GetGuestRoomResultEvent, GroupInformationComposer, GroupInformationEvent, GroupInformationParser,, HabboGroupDeactivatedMessageEvent, RoomEntryInfoMessageEvent } from '@nitro-rp/renderer';
+import { FC, useState } from 'react';import { GetGroupInformation, SendMessageComposer } from '../../../api';
+import { Base, Column, Flex, LayoutBadgeImageView, Text } from '../../../common';
+import { useMessageEvent } from '../../../hooks';
 
 export const GroupRoomInformationView: FC<{}> = props =>
 {
     const [ expectedGroupId, setExpectedGroupId ] = useState<number>(0);
     const [ groupInformation, setGroupInformation ] = useState<GroupInformationParser>(null);
-    const [ isOpen, setIsOpen ] = useState<boolean>(true);
-    const { showConfirm = null } = useNotification();
 
     useMessageEvent<DesktopViewEvent>(DesktopViewEvent, event =>
     {
@@ -61,61 +57,18 @@ export const GroupRoomInformationView: FC<{}> = props =>
         setGroupInformation(parser);
     });
 
-    const leaveGroup = () =>
-    {
-        showConfirm(LocalizeText('group.leaveconfirm.desc'), () =>
-        {
-            SendMessageComposer(new GroupRemoveMemberComposer(groupInformation.id, GetSessionDataManager().userId));
-        }, null);
-    }
-
-    const isRealOwner = (groupInformation && (groupInformation.ownerName === GetSessionDataManager().userName));
-
-    const getButtonText = () =>
-    {
-        if(isRealOwner) return 'group.manage';
-
-        if(groupInformation.state === GroupState.PRIVATE) return '';
-
-        if(groupInformation.membershipType === GroupMembershipType.MEMBER) return 'group.leave';
-
-        if((groupInformation.membershipType === GroupMembershipType.NOT_MEMBER) && groupInformation.state === GroupState.REGULAR) return 'group.join';
-
-        if(groupInformation.membershipType === GroupMembershipType.REQUEST_PENDING) return 'group.membershippending';
-
-        if((groupInformation.membershipType === GroupMembershipType.NOT_MEMBER) && groupInformation.state === GroupState.EXCLUSIVE) return 'group.requestmembership';
-    }
-
-    const handleButtonClick = () =>
-    {
-        if(isRealOwner) return GetGroupManager(groupInformation.id);
-
-        if((groupInformation.state === GroupState.PRIVATE) && (groupInformation.membershipType === GroupMembershipType.NOT_MEMBER)) return;
-
-        if(groupInformation.membershipType === GroupMembershipType.MEMBER)
-        {
-            leaveGroup();
-
-            return;
-        }
-
-        TryJoinGroup(groupInformation.id);
-    }
 
     if(!groupInformation) return null;
 
     return (
         <Base className="nitro-notification-bubble rounded">
             <Column>
-                { isOpen &&
-                    <>
-                        <Flex pointer alignItems="center" gap={ 2 } onClick={ event => GetGroupInformation(groupInformation.id) }>
-                            <Base className="group-badge">
-                                <LayoutBadgeImageView badgeCode={ groupInformation.badge } isGroup={ true } />
-                            </Base>
-                            <Text variant="white">{ groupInformation.title }</Text>
-                        </Flex>
-                    </> }
+                <Flex pointer alignItems="center" gap={ 2 } onClick={ event => GetGroupInformation(groupInformation.id) }>
+                    <Base className="group-badge">
+                        <LayoutBadgeImageView badgeCode={ groupInformation.badge } isGroup={ true } />
+                    </Base>
+                    <Text variant="white">{ groupInformation.title }</Text>
+                </Flex>
             </Column>
         </Base>
     );

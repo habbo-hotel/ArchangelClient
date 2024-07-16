efc6e91b56975a00bf71dca75372a957c5f8e2bd
chrismpettyjohn <chrismpettyjohn@gmail.com>
2024-07-15 22:20:24 -0400

    fix: hot bar list init

--- nitro-ui/src/api/roleplay/device/DeviceOpen.ts
+++ nitro-ui/src/api/roleplay/device/DeviceOpen.ts
@@ -2,6 +2,5 @@ import { DeviceOpenComposer } from '@nitro-rp/renderer';
 import { SendMessageComposer } from '../../';
 
 export function DeviceOpen(itemID: number): void {
-    console.log(itemID)
     SendMessageComposer(new DeviceOpenComposer(itemID));
 }
